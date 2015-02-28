0e8a4073c25791edfbf2d571acd05ee84b2ad190
angelgao <angel_gao@live.ca>
2015-02-28 02:58:12 +0000

    created rule controller with associated views

---
+++ app/controllers/rule_controller.rb
@@ -0,0 +1,49 @@
+  class RuleController < ApplicationController
+  
+  @@rules = [
+      [true, true, true, false],
+      [false, true, true, true],
+      [true, false, true, true],
+      [false, false, true, false],
+      [true, true, false, true],
+      [false, true, false, true],
+      [true, false, false, true],
+      [false, false, false, false]
+  ]
+  
+    def output
+  
+      three_thing_output = ->(left, center, right) do
+          @@rules.each do |rule|
+              if left == rule[0] and center == rule[1] and (right == rule[2] or (right.nil? and rule[2] == false))
+                  return rule[3]
+              end
+          end
+          return nil
+      end
+  
+      array_output = ->(arr, i) do
+          if i == 0
+              return three_thing_output[false, arr[i], arr[i+1]]
+          end
+          three_thing_output[arr[i-1], arr[i], arr[i+1]]
+      end
+  
+      
+      @result = []
+          @result << [true]
+          iterations = params["num"].to_i
+  
+          iterations.times do
+              current = @result.last
+              temp = []
+  
+              current.size.times do |i|
+                  temp << array_output[current, i]
+              end
+  
+              temp << true
+              @result << temp
+end
+      end
+    end
